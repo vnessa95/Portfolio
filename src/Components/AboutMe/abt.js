26524d4d296f14f0fc14be2c4dc6c7dfd18fb747
Vanessa <vnessabaker@gmail.com>
2020-07-06 15:00:44 -0400

    add

--- src/Components/AboutMe/abt.js
+++ src/Components/AboutMe/abt.js
@@ -15,7 +15,23 @@ export default class About extends Component {
                    <div className="p">
                        <h1 class="header">Hello (World)</h1>
                        <h2>I'm <span className="name">Vanessa Baker.</span></h2>
-                    <p className="me"> vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat</p>
+                    <p className="me"> I am a junior front end developer.<br/>
+                    my journey started about 6 months ago with <a href="https://roadtohire.org/"><span className="r2h">Road to Hire,</span></a> a 6 month internship dedicated to learning code and programming languages, and using it to enhance how to user interacts with the U/I. 
+                    i have always been driven by design and the creative side and i tend to express that in my work. I enjoy making responsive & dynamic websites/apps.<br/><br/><br/>
+                    <span className="langs">languages & frameworks:<br/>
+                        <ul className="skills">
+                            <li className="skill">&bull;CSS</li>
+                            <li className="skill">&bull;BOOTSTRAP</li>
+                            <li className="skill">&bull;Express</li>
+                            <li className="skill">&bull;TDD</li>
+                            <li className="skill">&bull;SASS/BEM/FLEXBOX</li>
+                            <li className="skill">&bull;node</li>
+                            <li className="skill">&bull;SQL</li>
+                            <li className="skill">&bull;JAVASCRIPT</li>
+                            <li className="skill">&bull;REACT.JS</li>
+                        </ul>
+                    </span>
+                      </p>
                     
                     <div className="icons">
                     <a href="https://www.linkedin.com/in/vanessa-baker-b80237181/" ><i id="fab" className="fab fa-linkedin-in fa-3x"></i></a>
