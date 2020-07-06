790f89a49033ec12d2dd4ca7800aad417951c12e
Vanessa <vnessabaker@gmail.com>
2020-07-06 10:29:40 -0400

    add

--- src/Components/AboutMe/abt.js
+++ src/Components/AboutMe/abt.js
@@ -18,9 +18,9 @@ export default class About extends Component {
                     <p className="me"> vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat</p>
                     
                     <div className="icons">
-                    <i href="https://www.linkedin.com/in/vanessa-baker-b80237181/" id="fab" className="fab fa-linkedin-in fa-3x"></i>
-                    <i href="https://www.instagram.com/thafloralyogi/" id="fab"  className="fab fa-instagram fa-3x"></i>
-                    <i href="https://github.com/vnessa95" id="fab"  className="fab fa-github fa-3x"></i>
+                    <a href="https://www.linkedin.com/in/vanessa-baker-b80237181/" ><i id="fab" className="fab fa-linkedin-in fa-3x"></i></a>
+                    <a href="https://www.instagram.com/thafloralyogi/"><i id="fab"  className="fab fa-instagram fa-3x"></i></a>
+                    <a href="https://github.com/vnessa95"><i id="fab"  className="fab fa-github fa-3x"></i></a>
                     </div>
                     </div>
             </div>
