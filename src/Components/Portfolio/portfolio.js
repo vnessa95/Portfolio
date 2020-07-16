197bd4db0cc09ab1abd9e67454c929a107b4cccd
Vanessa <vnessabaker@gmail.com>
2020-07-16 14:57:52 -0400

    add

--- src/Components/Portfolio/portfolio.js
+++ src/Components/Portfolio/portfolio.js
@@ -18,7 +18,7 @@ export default class Portfolio extends Component {
     <a href="https://my-1st-responsive-navbar.netlify.app/">
 <img className="image"  src='img/navbar.png' alt="navbarsite"></img>
 </a>
-<p className="txt"></p>
+<p className="txt">Responsive JS Navbar</p>
 <a href="https://github.com/vnessa95/jsnavbar" className="goto"><i id="i" className="fab fa-github-square fa-2x"></i></a>
 <i id="i" className="fab fa-node-js fa-2x"></i>
 </div>
@@ -27,7 +27,7 @@ export default class Portfolio extends Component {
     <a href="https://wikipedia-redesign.netlify.app/">
 <img className="image"  src='img/wiki.png' alt="wikipedia-redesign"></img>
 </a>
-<p className="txt"></p>
+<p className="txt">Wikipedia Redesign</p>
 <i id="i" className="fab fa-react fa-2x"></i>
 <a href="https://github.com/FatimaGarci/Redesign-Project" className="goto"><i id="i" className="fab fa-github-square fa-2x"></i></a>
 <i id="i" className="fab fa-node-js fa-2x"></i>
@@ -39,7 +39,7 @@ export default class Portfolio extends Component {
     <a href="https://we-rise.netlify.app/">
 <img className="image"  src='img/werise.png' alt="werise"></img>
 </a>
-<p className="txt"></p>
+<p className="txt">Mock "Hackathon"</p>
 <a href="https://github.com/vnessa95/werise" className="goto"><i id="i" className="fab fa-github-square fa-2x"></i></a>
 <i id="i" className="fab fa-node-js fa-2x"></i>
 </div>
@@ -49,7 +49,7 @@ export default class Portfolio extends Component {
     <a href="https://hotelvee.netlify.app/">
 <img className="image"  src='/img/hotelsite.png' alt="hotelsite"></img>
 </a>
-<p className="txt"></p>
+<p className="txt">Hotel App</p>
 <i id="i" className="fab fa-react fa-2x"></i>
 <a href="https://github.com/vnessa95/veeshotel" className="goto"><i id="i"  className="fab fa-github-square fa-2x"></i></a>
 <i id="i" className="fab fa-node-js fa-2x"></i>
@@ -59,7 +59,7 @@ export default class Portfolio extends Component {
     <a href="https://voice-rec-numbergame.netlify.app/">
 <img className="image"  src='/img/numgame.png' alt="numgamesite"></img>
 </a>
-<p className="txt"></p>
+<p className="txt">Voice Number Game</p>
 <a href="https://github.com/vnessa95/numberGame" className="goto"><i id="i" className="fab fa-github-square fa-2x"></i></a>
 <i id="i" className="fab fa-node-js fa-2x"></i>
 </div>
@@ -68,7 +68,7 @@ export default class Portfolio extends Component {
     <a href="https://veescounter.netlify.app/">
 <img className="image"  src='img/counter.png' alt="countersite"></img>
 </a>
-<p className="txt"></p>
+<p className="txt">Counter App</p>
 <i id="i" className="fab fa-react fa-2x"></i>
 <a href="https://github.com/vnessa95/counter" className="goto"><i id="i" className="fab fa-github-square fa-2x"></i></a>
 <i id="i" className="fab fa-node-js fa-2x"></i>
@@ -78,7 +78,7 @@ export default class Portfolio extends Component {
     <a href="https://vees-yoga.netlify.app/">
 <img className="image" src='/img/yogasite.png' alt="yogasite"></img>
 </a>
-<p className="txt"></p>
+<p className="txt">Yoga Products</p>
 <i id="i" className="fab fa-react fa-2x"></i>
 <a href="https://github.com/vnessa95/project4" className="goto"><i id="i"  className="fab fa-github-square fa-2x"></i></a>
 <i id="i" className="fab fa-node-js fa-2x"></i>
@@ -88,7 +88,7 @@ export default class Portfolio extends Component {
     <a href="https://veestictactoe.netlify.app/">
 <img className="image"  src='/img/tictak.png' alt="tiktaktoebaord"></img>
 </a>
-<p className="txt"></p>
+<p className="txt">Tik Tac Toe</p>
 <i id="i" className="fab fa-react fa-2x"></i>
 <a href="https://github.com/vnessa95/tic-tac_toe" className="goto"><i id="i" className="fab fa-github-square fa-2x"></i></a>
 <i id="i" className="fab fa-node-js fa-2x"></i>
@@ -99,7 +99,7 @@ export default class Portfolio extends Component {
     <a href="https://vees-todo.netlify.app/">
 <img className="image"  src='/img/to-do.png' alt="todolist"></img>
 </a>
-<p className="txt"></p>
+<p className="txt">ToDo List</p>
 <i id="i" className="fab fa-react fa-2x"></i>
 <a href="https://github.com/vnessa95/todo" className="goto"><i id="i" className="fab fa-github-square fa-2x"></i></a>
 <i id="i" className="fab fa-node-js fa-2x"></i>
